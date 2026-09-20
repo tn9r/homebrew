cad9609da5aaed6e74e3efe6e1e50d36773e2d93
Alpha <115908296+tn9r@users.noreply.github.com>
2026-09-20 10:17:30 +0800

    bump(kinetic): 0.4.1

--- Casks/kinetic.rb
+++ Casks/kinetic.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/custom_download_strategy"
 
 cask "kinetic" do
-  version "0.4.0"
+  version "0.4.1"
   sha256 :no_check
 
   url "https://github.com/narrino/kinetic/releases/download/v#{version}/Kinetic-#{version}.zip",
