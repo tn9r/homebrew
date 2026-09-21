63de86bc596c459263800d1ee4850f6874affed3
Alpha <115908296+tn9r@users.noreply.github.com>
2026-09-21 16:10:04 +0800

    bump(tacit): 0.10.2

--- Casks/tacit.rb
+++ Casks/tacit.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/custom_download_strategy"
 
 cask "tacit" do
-  version "0.10.1"
+  version "0.10.2"
   sha256 :no_check
 
   url "https://github.com/freetis/Tacit/releases/download/v#{version}/Tacit-#{version}.zip",
